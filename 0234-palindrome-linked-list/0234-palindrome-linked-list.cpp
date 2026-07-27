08d9be529cf0854ab8f47c3aa4e8e5bde611ca16
AYUSHI PANDEY <ayushipandey5926@gmail.com>
2026-07-27 23:13:51 +0530

    Time: 4 ms (49.22%), Space: 118.1 MB (65.39%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+    ListNode* prev = NULL;
+        while(head!=NULL) {
+            ListNode* temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+       /**vector<int> x;
+       for(ListNode* temp = head; temp!=NULL; temp=temp->next) {
+        x.push_back(temp->val);
+       }
+       
+       int left = 0, right = x.size()-1;
+       while(left<right) {
+       if(x[left]!=x[right]) return false;
+       left++;
+       right--;
+       }
+       return true;
+       */
+
+       if (!head || !head->next) return true;
+
+       ListNode* slow = head;
+       ListNode* fast = head;
+       while (fast->next != nullptr && fast->next->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        }
+
+
+       ListNode* secondHalf = reverseList(slow->next);
+
+       ListNode* L1 = head;
+       ListNode* L2 = secondHalf;
+
+       while(L2!=NULL) {
+       if(L1->val != L2->val) return false;
+       L1 = L1->next;
+       L2 = L2->next;
+       }
+       return true;
+    }
+
+};
