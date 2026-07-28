f5fb66b98944ff8d2d3df33c7614ddbce9c70834
AYUSHI PANDEY <ayushipandey5926@gmail.com>
2026-07-28 20:49:19 +0530

    Time: 15 ms (63.95%), Space: 56 MB (95.78%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:  
+
+    ListNode* findMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        }
+        return slow;
+    } 
+
+    ListNode* MergeList(ListNode* L1, ListNode* L2) {
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+
+        if(L1->val <= L2->val) {
+            head = L1;
+            L1 = L1->next;
+        } else {
+            head = L2;
+            L2 = L2->next;
+        }
+
+        tail = head;
+
+        while(L1 && L2) {
+            if(L1->val <= L2->val) {
+                tail->next = L1;
+                L1 = L1->next;
+            } else {
+                tail->next = L2;
+                L2 = L2->next;
+            }
+            tail = tail->next;
+        }
+
+        if(L1) {
+            tail->next = L1;
+        } else {
+            tail->next = L2;
+        }
+
+        return head; 
+    }
+
+
+    ListNode* sortList(ListNode* head) {
+
+        if (!head || !head->next) return head;
+
+        ListNode* mid = findMid(head);
+        ListNode* rightL = mid->next;
+        mid->next = NULL;
+
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(rightL);
+
+        return MergeList(left,right);
+    }
+};
