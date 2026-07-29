180968aa355cada76d9aad1568c7175ba808efa3
AYUSHI PANDEY <ayushipandey5926@gmail.com>
2026-07-29 19:06:29 +0530

    Time: 0 ms (100%), Space: 16.3 MB (95.61%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -10,47 +10,30 @@
  */
 class Solution {
 public:
-    
-    ListNode* reverseList(ListNode* head) {
-    ListNode* y = head;
-    ListNode* prev = NULL;
-    while(y != NULL) {
-        ListNode* temp = y->next;
-        y->next = prev;
-        prev = y;
-        y = temp;
-    } 
-    return prev;
-}
+
 
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL) return head;
+        
+        int count = 1;
+        ListNode* temp = head;
+        while(temp->next!=NULL) {
+            temp=temp->next;
+            count++;
+        }
 
-    int count = 1;
-    ListNode* temp = head;
-    while(temp->next != NULL) {
-        temp = temp->next;
-        count++;
-    }
+        k = k%count;
+        if(k==0) return head;
+        temp->next = head;
 
-    k = k % count;
-    if(k == 0) return head;
+        ListNode* newTail = head;
+        for(int i=0; i<count-k-1; i++) {
+            newTail = newTail->next;
+        }
 
-    
-    ListNode* splitPrev = head;
-    for(int i = 1; i < count - k; i++) {
-        splitPrev = splitPrev->next;
-    }
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
 
-    ListNode* L = splitPrev->next;   
-    splitPrev->next = NULL;          
-
-    ListNode* L1 = reverseList(head); 
-    ListNode* L2 = reverseList(L);     
-
-    head->next = L2;   
-
-    ListNode* result = reverseList(L1);  
-    return result;
+        return newHead;
     }
 };
