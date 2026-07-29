188653580fedd3cbd3fa8521f567e5a0b5abe4cf
AYUSHI PANDEY <ayushipandey5926@gmail.com>
2026-07-29 17:27:12 +0530

    Time: 0 ms (100%), Space: 16.6 MB (6.41%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,48 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = NULL;
 
-        while(curr!=NULL) {
-            ListNode* temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        return prev;
-    }
-
-    ListNode* getKthNode(ListNode* temp, int k) {
-        k -= 1;
-        while(temp!=NULL && k>0) {
-            k--;
-            temp = temp->next;
-        }
-        return temp;
-    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode* temp = head;
-        ListNode* prevNode = NULL;
-        while(temp!=NULL) {
-            ListNode* KthNode = getKthNode(temp,k);
-            if(KthNode == NULL) {
-                if(prevNode) prevNode->next = temp;
-                break;
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prevNode = dummy;
+
+        while(true) {
+            ListNode* kth = prevNode;
+            for(int i=0; i<k && kth != NULL; i++) {
+                kth = kth->next;
+            } 
+            if(kth == NULL) break;
+
+            ListNode* nextNode = kth->next;
+            ListNode* prev = nextNode;
+            ListNode* curr = prevNode->next;
+
+            while(curr!=nextNode) {
+                ListNode* temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
             }
 
-            ListNode* nextNode = KthNode->next;
-            KthNode->next = NULL;
-            reverseList(temp);
-            if(temp == head) {
-                head = KthNode;
-            } else {
-                prevNode->next = KthNode;
-            }
+            ListNode* temp = prevNode->next;
+            prevNode->next = kth;
             prevNode = temp;
-            temp = nextNode;
         }
-        return head;
+        return dummy->next;
     }
 };
